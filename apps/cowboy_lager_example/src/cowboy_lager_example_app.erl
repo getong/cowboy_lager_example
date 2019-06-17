5e803433123befc291eedb3568aa8fdf8fd964bc
19年梦醒 <3949379+getong@users.noreply.github.com>
2019-06-17 11:44:30 +0800

    添加cowboy启动和回调模块

--- apps/cowboy_lager_example/src/cowboy_lager_example_app.erl
+++ apps/cowboy_lager_example/src/cowboy_lager_example_app.erl
@@ -15,6 +15,14 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+		{'_', [
+			{"/", toppage_h, []}
+		]}
+	]),
+	{ok, _} = cowboy:start_clear(http, [{port, 18080}], #{
+		env => #{dispatch => Dispatch}
+	}),
     cowboy_lager_example_sup:start_link().
 
 %%--------------------------------------------------------------------
